181d6f7c15a6b22d63085897028a7b341cc86216
Jagdeep Singh <jagdeepkh@gmail.com>
2015-02-09 09:51:30 +0530

    more changes

--- app/models/batch.rb
+++ app/models/batch.rb
@@ -1,4 +1,6 @@
 class Batch < ActiveRecord::Base
+  has_and_belongs_to_many :exams
+  has_and_belongs_to_many :papers
   has_many :subscriptions
   has_many :students, :through => :subscriptions
   belongs_to :account
