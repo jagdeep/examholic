84b44c1fec7e2ff508d30f041b0070e24ee65644
Jagdeep Singh <jagdeepkh@gmail.com>
2015-02-09 09:29:49 +0530

    minor changes

--- app/controllers/exams_controller.rb
+++ app/controllers/exams_controller.rb
@@ -26,6 +26,7 @@ class ExamsController < ApplicationController
   # POST /exams.json
   def create
     @exam = Exam.new(exam_params)
+    @exam.account_id = 1
 
     respond_to do |format|
       if @exam.save
@@ -70,6 +71,6 @@ class ExamsController < ApplicationController
 
     # Never trust parameters from the scary internet, only allow the white list through.
     def exam_params
-      params[:exam]
+      params.require(:exam).permit(:name,:duration,:start_time,:end_time)
     end
 end
