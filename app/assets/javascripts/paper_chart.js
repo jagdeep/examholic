d18936f3003bff8ddf53ae8866dca9004ff2525d
Trainee1 <trainee1@maropost10s-Mac-mini.local>
2016-11-24 12:08:09 +0530

    static array of colors

--- app/assets/javascripts/paper_chart.js
+++ app/assets/javascripts/paper_chart.js
@@ -1,16 +1,16 @@
 function paperPieChart(id, labels, data){
   var ctx = document.getElementById(id);
-  var borders = [];
-  var colors = [];
-  for(i=0; i<labels.length; i++){
-    colors.push(dynamicColors());
-    borders.push(dynamicHovers());
-  }
+  // var borders = [];
+//   var colors = [];
+//   for(i=0; i<labels.length; i++){
+//     colors.push(dynamicColors());
+//     borders.push(dynamicHovers());
+//   }
   var data = {
     labels: labels,
     datasets: [{ data: data, 
-      backgroundColor: colors, 
-      hoverBackgroundColor: borders
+      backgroundColor: dynamicColors, 
+      hoverBackgroundColor: dynamicHovers
     }],
   };
   var myPieChart = new Chart(ctx, {
